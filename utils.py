32a3badf67a7b0a008de257d3e4a2280d9e63920
fatemeh soleymani <56516238+fatislm@users.noreply.github.com>
2024-08-26 02:13:04 +0330

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,7 +1,8 @@
 import torch
+import numpy as np
 from torchvision import transforms
 from PIL import Image
-from loss_functions import loss_func
+from loss_functions import loss_func, mse
 
 def transform_image(image_path):
     transform = transforms.Compose([
@@ -20,20 +21,27 @@ def add_noise(x, noise_level, noise_type):
     return noisy
 
 def train(model, optimizer, noisy_img):
-    loss = loss_func(noisy_img, model)
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
-    return loss.item()
+
+  loss = loss_func(noisy_img, model)
+
+  optimizer.zero_grad()
+  loss.backward()
+  optimizer.step()
+
+  return loss.item()
 
 def test(model, noisy_img, clean_img):
+
     with torch.no_grad():
-        pred = torch.clamp(noisy_img - model(noisy_img), 0, 1)
+        pred = torch.clamp(noisy_img - model(noisy_img),0,1)
         MSE = mse(clean_img, pred).item()
-        PSNR = 10 * torch.log10(1 / MSE)
+        PSNR = 10*np.log10(1/MSE)
+
     return PSNR
 
 def denoise(model, noisy_img):
+
     with torch.no_grad():
-        pred = torch.clamp(noisy_img - model(noisy_img), 0, 1)
+        pred = torch.clamp( noisy_img - model(noisy_img),0,1)
+
     return pred
